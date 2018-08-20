3fba23085888b520b99517f8bc34353edca4844b
Diego Mosan <diego14290764@itcg.edu.mx>
2018-08-20 15:16:23 -0500

    The director was changed.

---
+++ src/interfaces/loopback-response.interface.ts
@@ -0,0 +1,9 @@
+export interface LoopbackResponseInterface {
+    id?: string;
+    ttl?: number;
+    created?: string;
+    userId?: string;
+    error?: any;
+    email?: string;
+    repassword?: string;
+}
