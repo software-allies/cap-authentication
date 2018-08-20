cbb6ffe73a69bd71a8c9d8275f251c6f065035b9
Diego Mosan <diego14290764@itcg.edu.mx>
2018-08-20 15:17:28 -0500

    The properties facebookId and googleId were added.

--- src/services/config.service.ts
+++ src/services/config.service.ts
@@ -1,17 +1,19 @@
 import { Injectable, Optional } from '@angular/core';
 
-
 @Injectable()
 export class ConfigService {
-
+    
     apiUrl: string;
     loginEndpoint: string;
+    facebookId: string;
+    googleId: string;
 
     constructor(@Optional() config: ConfigService) {
         if (config) { 
             this.apiUrl = config.apiUrl;
             this.loginEndpoint = config.loginEndpoint;
-        }
+            this.facebookId = config.facebookId;
+            this.googleId = config.googleId;
+        } 
     }
-
 }
