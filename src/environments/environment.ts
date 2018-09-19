9a3cfb3c656b84e969ca8bed7b74bfff580d1d90
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2018-09-19 09:10:15 -0500

    create the file enviroments for the firebase requirements and credentials

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment ={
+    production: false,
+    firebase: {
+    apiKey: "AIzaSyAJvwLYR8nn_4nWNnWW-B06qUXtx_HNMWk",
+    authDomain: "notifications-ffb75.firebaseapp.com",
+    databaseURL: "https://notifications-ffb75.firebaseio.com",
+    projectId: "notifications-ffb75",
+    storageBucket: "notifications-ffb75.appspot.com",
+    messagingSenderId: "798294453386"
+    }
+}
