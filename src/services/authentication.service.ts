9494cbc3778161c6022c7207e02a57d200cd31af
Diego Mosan <diego14290764@itcg.edu.mx>
2018-09-24 15:51:58 -0500

    The validation for the function getUserData was fixed.

--- src/services/authentication.service.ts
+++ src/services/authentication.service.ts
@@ -30,6 +30,7 @@ export class AuthenticationService {
         };
         this.actionUrl = `${this._config.apiUrl}${this._config.loginEndpoint}`;
         this._token = localStorage.getItem('token') || '';
+        console.log('this._token: ', this._token);
     }
 
     addRegister(credentials: CredentialsInterface){ 
@@ -145,7 +146,7 @@ export class AuthenticationService {
     }
 
     getUserData():any{
-        if(this.userData.length !== 0){
+        if(this.userData !== null){
             return this.userData;
         }
     }
