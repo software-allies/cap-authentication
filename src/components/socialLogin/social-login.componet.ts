fbb4710df37982530f160e5b79b212c880b28d63
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2018-10-09 12:56:47 -0500

    src/components/socialLogin/social-login.componet.ts create a new way to login with different social media, both browser and mobile

--- src/components/socialLogin/social-login.componet.ts
+++ src/components/socialLogin/social-login.componet.ts
@@ -1,58 +1,112 @@
 // import { SocialMediaService } from '../../services/social-media.service';
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../../services/authentication.service';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireDatabase } from  '@angular/fire/database';
+import firebase from 'firebase/app';
 
-import { AuthService, FacebookLoginProvider, GoogleLoginProvider, AuthServiceConfig } from 'angular5-social-login';
-import { ConfigService } from '../../services/config.service';
+import { Platform } from 'ionic-angular';
+import { Facebook } from '@ionic-native/facebook';
+import { SocialMediaInterface } from './../../interfaces/socialMedia.interface';
 
 @Component({
     selector: 'social-login',
     template: 
     `
-    <div *ngIf="socialMedia">
-      <div *ngFor="let social of socialMedia">
-        <button ion-button color="{{social.color}}" type="submit" block secondary (click)="socialSignIn(social.name)" icon-end>
-          {{ social.name }}    
-          <ion-icon md="logo-{{ social.icon }}" name="logo-{{ social.icon }}" item-right></ion-icon> 
-        </button>
-      </div>
-    </div>
+    <button ion-button color="blue" type="submit" block secondary (click)="signInWithFacebook()" icon-end>
+       Login with Facebook  
+    </button>
+    <button ion-button color="danger" type="submit" block secondary (click)="signInWithGoogle()" icon-end>
+       Login with Google  
+    </button>
     `,
-    // 
     
 })
 
 export class SocialLoginComponent implements OnInit {
-  socialMedia:any[] = [
-    {
-      name: 'Facebook',
-      color: 'blue',
-      icon: 'facebook'
-    },
-    {
-      name: 'Google',
-      color: 'danger',
-      icon: 'googleplus'
-    }
-  ];
-  constructor( private socialAuthService: AuthService, private authenticationService:AuthenticationService ) { }
 
-  ngOnInit() { }
-
-  socialSignIn(socialPlatform : string) {
-      // console.log('socialPlatform: ', socialPlatform);
-      let socialPlatformProvider:any;
-      if(socialPlatform == "Facebook"){
-        socialPlatformProvider = FacebookLoginProvider.PROVIDER_ID;
-      }else if(socialPlatform == "Google"){
-        socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
-      }
-      
-      this.socialAuthService.signIn(socialPlatformProvider).then(
-        (userData) => {
-          this.authenticationService.isLoggedIn = true;
-          this.authenticationService.saveSocialMediaData(userData);
-        }
-      )
+  credentials : SocialMediaInterface = {
+    username:'',
+    email: '',
   }
+  displayName:string; 
+
+  constructor(private authenticationService : AuthenticationService,
+              private angularFireauth: AngularFireAuth,
+              private facebook: Facebook,
+              private platform: Platform,
+              private angularFireDatabase:AngularFireDatabase,
+              ){}
+
+  ngOnInit() {}
+
+  signInWithFacebook(){
+    if (this.platform.is('cordova')) {
+      this.nativeFacebookLogin();
+    } else {
+      this.webFacobookLogin();
+    }
+  }
+
+  signInWithGoogle(){
+    if (this.platform.is('cordova')) {
+      this.webGoogleLogin();
+    } else {
+      this.webGoogleLogin();
+    }
+  }
+
+//Authentication
+  authCordova(){
+    this.angularFireauth.authState.subscribe(user=>{
+      if(user){
+        this.credentials={
+          username:user.displayName,
+          email: user.email,
+        }
+        this.authenticationService.addRegisterSocialMedia(this.credentials);
+      }
+    })
+  }
+
+  authBrowser(){
+    this.angularFireauth.authState.subscribe(user =>{
+      if(user){
+        this.credentials = {
+        email:user.email,
+        username:user.displayName,
+      }
+      const db = this.angularFireDatabase.database.ref('Browser');
+      db.push(this.credentials);
+      }
+    })
+  }
+
+  //Browser
+  webGoogleLogin() {
+    this.angularFireauth.auth
+      .signInWithPopup(new firebase.auth.GoogleAuthProvider())
+      .then( res => console.log(res)
+      )
+      this.authBrowser();
+  }
+
+  webFacobookLogin(){
+    this.angularFireauth.auth
+    .signInWithPopup(new firebase.auth.FacebookAuthProvider())
+    .then(res => console.log(res));
+    this.authBrowser();
+  }
+
+  //Native
+  nativeFacebookLogin(){
+    this.facebook.login(['email', 'public_profile']).then(res=>{
+    const facebookCredential = firebase.auth.FacebookAuthProvider.credential(res.authResponse.accessToken);
+    firebase.auth().signInWithCredential(facebookCredential);    
+    })
+    this.authCordova();
+  }
+
+  nativeGoogleLogin(){}
+
 }
