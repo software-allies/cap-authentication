edd63c22230301a1ca528c546d62c780313dc65a
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2018-10-09 13:02:51 -0500

    src/services/config.service.ts delete unnecessary configurations

--- src/services/config.service.ts
+++ src/services/config.service.ts
@@ -4,20 +4,16 @@ import { Injectable, Optional } from '@angular/core';
 
 @Injectable()
 export class ConfigService {
-    
+
     apiUrl: string;
     loginEndpoint: string;
-    facebookId: string;
-    googleId: string;
-    firebase:any;
+
 
     constructor(@Optional() config: IConfig) {
-        if (config) { 
+        if (config) {
             this.apiUrl = config.apiUrl;
             this.loginEndpoint = config.loginEndpoint;
-            this.facebookId = config.facebookId;
-            this.googleId = config.googleId;
-            this.firebase = config.firebase;
-        } 
+            
+        }
     }
 }
