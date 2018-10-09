1bc253111390b6ef89fa6a80592e19cd7ff298fd
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2018-10-09 12:59:38 -0500

    src/interfaces/config.interface.ts delete old configurations

--- src/interfaces/config.interface.ts
+++ src/interfaces/config.interface.ts
@@ -1,14 +1,5 @@
 export interface IConfig {
     apiUrl: string;
     loginEndpoint: string;
-    facebookId: string;
-    googleId: string;
-    firebase?: {
-        apiKey: string;
-        authDomain: string;
-        databaseURL: string;
-        projectId: string;
-        storageBucket?: string;
-        messagingSenderId?: string;
-    }
+    
 }
