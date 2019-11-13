310265449ca6e6fce95232b2ca5266163e6f8311
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2019-11-13 10:43:55 -0600

    CAP-94 src/components/login/login.component.ts Create a new view for the user at the time of logging in.

--- src/components/login/login.component.ts
+++ src/components/login/login.component.ts
@@ -1,79 +1,188 @@
-import { Component, Output, EventEmitter, ViewEncapsulation } from '@angular/core'; 
-import { FormGroup, FormBuilder, Validators } from "@angular/forms";
-import { CredentialsInterface } from './../../interfaces/credentials.interface';
+import { Component, ViewEncapsulation, Inject, PLATFORM_ID  } from '@angular/core';
+import { FormGroup, FormControl, Validators, } from '@angular/forms';
 import { AuthenticationService } from '../../services/authentication.service';
+import { isPlatformBrowser } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
-    selector: "auth-app-login",
-    template: ` 
-<ion-grid>
-   <ion-row>
-     <ion-col col-12 col-xl-8 offset-xl-2 col-lg-10 offset-lg-1>
-        <ion-list>
-            <form [formGroup]="loginform" (ngSubmit)="onSubmit()">
-            <ion-item>
-                <ion-label stacked primary>Email</ion-label>
-                <ion-input [(ngModel)]="credentials.email" formControlName="email"
-                        type="text" id="email" spellcheck="false" autocapitalize="off" ngDefaultControl>
-                </ion-input>
-            </ion-item>
-            <ion-item>
-                <ion-label stacked primary>Password</ion-label>
-                <ion-input [(ngModel)]="credentials.password" formControlName="password" type="text" id="password" ngDefaultControl>
-                </ion-input>
-            </ion-item>
-            <button ion-button type="submit" block primary [disabled]="!loginform.valid">Login</button>
-            <button ion-button type="submit" block secondary (click)="createAccount()">Create an account</button>
-            <social-login></social-login>
-            </form>
-        </ion-list>
-     </ion-col>
-   </ion-row>
- </ion-grid>
-        `,
-    styles: [``],
+  selector: "auth-app-login",
+  template: `
+  <div class="container register-form">
+    <div class="form">
+      <div class="header">
+          <p>Application Name</p>
+      </div>
+      <div class="form-content">
+        <form [formGroup]="loginUserForm" (ngSubmit)="loginUser()">
+          <div class="row">
+            <div class="col-md-6">
+                <div class="form-group" >
+                    <input  type="text"
+                            class="form-control"
+                            placeholder="Email address *"
+                            [ngClass]="{'is-invalid':!loginUserForm.get('email').valid && loginUserForm.get('email').touched}"
+                            formControlName="email"/>
+                </div>
+                <div class="form-group">
+                    <input  type="password"
+                            class="form-control"
+                            placeholder="Password *"
+                            [ngClass]="{'is-invalid':!loginUserForm.get('password').valid && loginUserForm.get('password').touched}"
+                            formControlName="password"/>
+                    <small *ngIf="!loginUserForm.get('password').valid && loginUserForm.get('password').touched" class="form-text text-center text-muted">
+                      Your password must be 8-20 characters long, contain letters and numbers and the first letter has to be uppercase.
+                    </small>
+
+                  <div *ngIf="userNotValid"  class="form-control-feeback text-danger text-center">
+                      This user already exists, try other alternate data
+                  </div>
+              </div>
+            </div>
+              <div class="col-md-6">
+                <div class="form-group">
+                  <button type="submit"
+                          class="btnSubmit"
+                          [disabled]="!loginUserForm.valid">Email</button>
+                </div>
+                <div class="form-group">
+                  <button (click)="signInSocialMedia(true)" type="button" class="btnFacebook ">Facebook</button>
+                </div>
+
+                <div class="form-group">
+                <button (click)="signInSocialMedia(false)" type="button" class="btnGoogle ">Google</button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+
+  `,
+  styles: [`
+.header
+{
+  text-align: center;
+  height: 80px;
+  background: black;
+  color: #fff;
+  font-weight: bold;
+  line-height: 80px;
+}
+.form-content
+{
+  padding: 5%;
+  border: 1px solid #ced4da;
+  margin-bottom: 2%;
+}
+.form-control{
+  border-radius:1.5rem;
+}
+.btnSubmit
+{
+  border:none;
+  border-radius:1.5rem;
+  padding: 1%;
+  width: 100%;
+  cursor: pointer;
+  background: black;
+  color: #fff;
+}
+.btnFacebook
+{
+  border:none;
+  border-radius:1.5rem;
+  padding: 1%;
+  width: 100%;
+  cursor: pointer;
+  background: #0000FF;
+  color: #fff;
+}
+.btnGoogle
+{
+  border:none;
+  border-radius:1.5rem;
+  padding: 1%;
+  width: 100%;
+  cursor: pointer;
+  background: #FF0000;
+  color: #fff;
+}
+  `],
   encapsulation: ViewEncapsulation.Emulated
 })
 export class AuthLoginComponent {
 
-    @Output()
-    submit: EventEmitter<any> = new EventEmitter();
+  loginUserForm: FormGroup;
+  userNotValid: boolean;
 
-    @Output()
-    changePage: EventEmitter<boolean> = new EventEmitter();
+  constructor(
+    private authenticationService: AuthenticationService,
+    @Inject(PLATFORM_ID) private platformId,
+    private router: Router
+  ) {
+    this.loginUserForm = new FormGroup({
+      'email': new FormControl('', [Validators.required]),
+      'password': new FormControl('', [Validators.required])
+    });
+    this.userNotValid = false;
+  }
 
-    credentials: CredentialsInterface = { 
-        email: '', 
-        password: '' 
-    };
+  ngOnInit() { }
 
-    loginform: FormGroup;
+  loginUser() {
+    this.authenticationService.authWithEmail(this.loginUserForm.value)
+    .then((response) => {
+      response.user.getIdTokenResult().then((res) => {
+        if (isPlatformBrowser(this.platformId)) {
+          localStorage.setItem('User', JSON.stringify({
+            uid: response.user.uid,
+            user: response.user.email.split('@', 1)[0],
+            email: response.user.email,
+            refresh_token: response.user.refreshToken,
+            token: res.token
+          }));
+          this.router.navigate(['/']);
+        }
+      });
+    }).catch(error => this.userNotValid = true);   }
 
-    constructor(
-        private authenticationService: AuthenticationService,
-        public formBuilder: FormBuilder,
-
-    ){
-    }
-  
-    ngOnInit(): any {
-        this.loginform = this.formBuilder.group({
-            password: ['', [Validators.required, Validators.minLength(3)]],
-            email: ['', Validators.required]
+  signInSocialMedia(socialMedia: boolean) {
+    if (socialMedia) {
+      this.authenticationService.authWithFacebook().then((response) => {
+        response.user.getIdTokenResult().then((res) => {
+          if (isPlatformBrowser(this.platformId)) {
+            localStorage.setItem('User', JSON.stringify({
+              user: response.user.email.split('@', 1)[0],
+              email: response.user.email,
+              refresh_token: response.user.refreshToken,
+              token: res.token
+            }));
+          }
+        }).then(() => {
+          this.authenticationService.currentUser.subscribe((user) => {
+          })
+          // this.router.navigate(['/']);
         });
+      }).catch(error => this.userNotValid = true);
+
+
+    } else {
+      this.authenticationService.authWithGoogle().then((response) =>  {
+        response.user.getIdTokenResult().then((res) => {
+          if (isPlatformBrowser(this.platformId)) {
+            localStorage.setItem('User', JSON.stringify({
+              user: response.user.email.split('@', 1)[0],
+              email: response.user.email,
+              refresh_token: response.user.refreshToken,
+              token: res.token
+            }));
+          }
+        }).then(() => {
+          this.router.navigate(['/']);
+        });
+      }).catch(error => this.userNotValid = true);
     }
-
-    onSubmit() {
-        this.authenticationService
-            .login(this.credentials)
-            .subscribe((result: any) => {
-                this.submit.emit(result);
-            });
-    }
-
-    createAccount() {
-        this.changePage.emit(true);
-    }   
-
+  }
 }
 
