2e15a56e48ff04498b7ba14e0e23eda39ae24af2
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2019-11-13 10:33:55 -0600

    CAP-94 src/services/authentication.service.ts Create all the necessary requests so that Firebase can provide the authentication services.

--- src/services/authentication.service.ts
+++ src/services/authentication.service.ts
@@ -1,186 +1,56 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, pipe } from 'rxjs';
+import { Observable } from 'rxjs';
 import { ConfigService } from './config.service';
-import { map, catchError, tap } from 'rxjs/operators';
-import { CredentialsInterface } from '../interfaces/credentials.interface';
-import { SocialMediaInterface } from '../interfaces/socialMedia.interface';
-import { LoopbackResponseInterface } from '../interfaces/loopback-response.interface';
-
-import { AngularFireDatabase } from  '@angular/fire/database';
-import { FCM } from '@ionic-native/fcm';
-import 'rxjs/Observable';
-import 'rxjs/add/observable/throw';
+import * as firebase from 'firebase/app';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Injectable()
 export class AuthenticationService {
-    userId: string = '';
-    _token: string;
-    isLoggedIn: boolean = false;
-    userData: any = {};
 
-    private actionUrl: string;
-    private httpOptions: any;
- 
-    constructor(
-        private _http: HttpClient, 
-        private _config: ConfigService,
-        private angularFireDatabase: AngularFireDatabase,
-        public fcm:FCM,
-    ) {
-        this.httpOptions = {
-            headers: new HttpHeaders({ 
-                'Content-Type': 'application/json'
-            })
-        };
-        this.actionUrl = `${this._config.apiUrl}${this._config.loginEndpoint}`;
-        this._token = localStorage.getItem('token') || '';
-        console.log('this._token: ', this._token);
-    }
+  private user: Observable<firebase.User | null>;
 
-    RegisterMobile(credentials: CredentialsInterface){ 
-        const newValues = {
-            username: credentials.username,
-            email: credentials.email,
-            rol: credentials.rol,
-        }
-        this.fcm.getToken().then(token=>{
-            let _token_ = {token:token};
-            const Values = Object.assign(newValues,_token_);
-            const db = this.angularFireDatabase.database.ref('Devices');
-            db.push(Values);
-        })
-    }
+  constructor(private afAuth: AngularFireAuth) {
+    this.user = this.afAuth.authState;
+  }
 
-    RegisterBrowser(credentials: CredentialsInterface){
-        const newValues = {
-            username: credentials.username,
-            email: credentials.email,
-            rol: credentials.rol,
-        }
-        const db = this.angularFireDatabase.database.ref('Browser');
-            db.push(newValues);
-    }
+  get authenticated(): boolean {
+    return this.user != null;
+  }
 
-    RegisterSocialMediaMobile(credentials: SocialMediaInterface){
-        this.fcm.getToken().then(token=>{
-            let _token_ = {token:token};  
-            const newValues = Object.assign(credentials,_token_);
-            const db = this.angularFireDatabase.database.ref('Devices');
-            db.push(newValues);
-        })
-    }
+  get currentUser(): Observable<firebase.User | null> {
+    return this.user;
+  }
 
-    RegisterSocialMediaBrowser(credentials: SocialMediaInterface){
-        const db = this.angularFireDatabase.database.ref('Browser');
-        db.push(credentials);
-    }
-    
-    login(credentials: CredentialsInterface) {
-        
-        const toAdd = JSON.stringify(credentials);
+  signOut(): Promise<void> {
+    return this.afAuth.auth.signOut();
+  }
 
-        return this._http.post<LoopbackResponseInterface>(`${this.actionUrl}/login`, toAdd, this.httpOptions)
-            .pipe(
-                map(response => response),
-                catchError(this.handleError),
-                tap((response: any) => {
-                    if (response.id !== undefined) {
-                        this.isLoggedIn = true;
-                        this._token = response.id;
-                        localStorage.setItem('isLoggedIn', 'true'); 
-                        localStorage.setItem('userId', response.userId || '');
-                        localStorage.setItem('created', response.created || '');
-                        localStorage.setItem('token', response.id);
-                        localStorage.setItem('email', response.email || '');
-                    } 
-                    return response;
-                })
-            );
-    }
+  createUser(user: any): Promise<firebase.auth.UserCredential> {
+    return this.afAuth.auth.createUserWithEmailAndPassword(user.email, user.password);
+  }
 
-    changePassword(credentials: CredentialsInterface) {
-        const newValues = {
-            oldPassword: credentials.password,
-            newPassword: credentials.repassword
-        }
-        const toAdd = JSON.stringify(newValues);
+  authWithEmail(user: any): Promise<firebase.auth.UserCredential> {
+    return this.afAuth.auth.signInWithEmailAndPassword(user.email, user.password);
+  }
 
-        return this._http.post<any>(`${this.actionUrl}/change-password?access_token=${this._token}`, toAdd, this.httpOptions)
-            .pipe(
-                map(response => response),
-                catchError(this.handleError),
-                tap((response: any) => {
-                    return response;
-                })
-            );
-    }
+  authWithFacebook(): Promise<firebase.auth.UserCredential> {
+    const provider: firebase.auth.FacebookAuthProvider = new firebase.auth.FacebookAuthProvider();
+    provider.addScope('user_birthday');
+    return this.afAuth.auth.signInWithPopup(provider);
+  }
 
-    register(credentials: CredentialsInterface) {
-        
-        const toAdd = JSON.stringify(credentials);
+  authWithGoogle(): Promise<firebase.auth.UserCredential> {
+    const provider: firebase.auth.GoogleAuthProvider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+    return this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
 
-        return this._http.post<LoopbackResponseInterface>(`${this.actionUrl}/`, toAdd, this.httpOptions)
-            .pipe(
-                map(response => response),
-                catchError(this.handleError),
-                tap((response: any) => {
-                    return response;
-                })
-            );
-    }
+  updateProfile(user: any): Promise<void> {
+    return this.afAuth.auth.updateCurrentUser(user);
+  }
 
-    editProfile(credentials: CredentialsInterface) {
-        const newValues = {
-            newUsername: credentials.username
-        }
-        const toAdd = JSON.stringify(newValues);
-
-        return this._http.put<any>(`${this.actionUrl}/${this.userId}`, toAdd, this.httpOptions)
-        .pipe(
-                map(response => response),
-                catchError(this.handleError),
-                tap((response: any) => {
-                    return response;
-                })
-            );
-    }
-
-    logout(): void {
-        this.isLoggedIn = false;
-        this._token = '';
-        localStorage.removeItem('isLoggedIn');
-        localStorage.removeItem('token');
-        localStorage.removeItem('userId');
-        localStorage.removeItem('created');
-        console.log('logout', 'Has cerrado sesión correctamente');
-    }
-    
-    token(): string {
-        return this._token;
-    }
-        
-    isAuthenticated() {
-        if (this.isLoggedIn) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    
-    private handleError(error: any) {
-        console.log(error);
-        return Observable.throw(error || 'Server error');
-    }
-
-    saveSocialMediaData(data:any){
-        this.userData = data;
-    }
-
-    getUserData():any{
-        if(this.userData !== null){
-            return this.userData;
-        }
-    }
+  resetPassword(email: string): Promise<void> {
+    return this.afAuth.auth.sendPasswordResetEmail(email);
+  }
 
 }
