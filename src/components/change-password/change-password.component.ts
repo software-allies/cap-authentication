e3bbf2000c8c868e4db3724fdacf895c780ea0ae
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2019-11-13 10:45:26 -0600

    CAP-94 src/components/change-password/change-password.component.ts

--- src/components/change-password/change-password.component.ts
+++ src/components/change-password/change-password.component.ts
@@ -1,80 +1,103 @@
 import { Component, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl, Validators } from "@angular/forms";
-import { CredentialsInterface } from '../../interfaces/credentials.interface';
-
-// import { AuthenticationService } from 'authmodule-angular6-module-example';
 import { AuthenticationService } from '../../services/authentication.service';
+import { Router } from '@angular/router';
 
-@Component({ 
+@Component({
   selector: "auth-app-change-password",
   template: `
-<ion-grid>
-   <ion-row>
-     <ion-col  col-12 col-xl-8 offset-xl-2 col-lg-10 offset-lg-1>
-        <ion-list>
-            <form [formGroup]="changeform" (ngSubmit)="onSubmit()">
-            <ion-item>
-                <ion-label stacked primary>Password</ion-label>
-                <ion-input [(ngModel)]="credentials.password" formControlName="password" type="text" id="password" ngDefaultControl>
-                </ion-input>
-            </ion-item>
-            <ion-item>
-                <ion-label stacked primary>Repeat Password</ion-label>
-                <ion-input [(ngModel)]="credentials.repassword" formControlName="repassword" type="text" id="repassword" ngDefaultControl>
-                </ion-input>
-            </ion-item>
-            <button ion-button type="submit" block full primary [disabled]="!changeform.valid">Change Password</button>
-            <button ion-button type="submit" block secondary (click)="loginAccount()">Login to account</button>
-            </form>
-        </ion-list>
-     </ion-col>
-   </ion-row>
- </ion-grid>
+  <div class="container register-form">
+    <div class="form">
+      <div class="header">
+          <p>Forgot Password</p>
+      </div>
+
+      <form [formGroup]="changeform" (ngSubmit)="forgorPassword()">
+        <div class="form-content">
+          <div class="form-group d-flex justify-content-center row">
+            <label *ngIf="!emailSend" class="col-12  text-center col-form-label">
+              Enter your email address and we will send you a link to reset your password.
+            </label>
+            <label *ngIf="emailSend" class="col-12  text-center col-form-label">
+              An e-mail was sent to your email address that you provided, there you can change your password.
+            </label>
+            <div class="col-4">
+              <input  *ngIf="!emailSend"
+                      class="form-control"
+                      type="email"
+                      placeholder="Email address"
+                      formControlName="email">
+              <small id="passwordHelpBlock" class="form-text text-center text-muted">
+              </small>
+              <button *ngIf="!emailSend" type="submit" class="btnSubmit">Edit Profile</button>
+              <button *ngIf="emailSend" type="button"  (click)="goToHome()" class="btnSubmit">Go to Home</button>
+            </div>
+          </div>
+        </div>
+      </form>
+
+    </div>
+  </div>
   `,
-  styles: [``],
+  styles: [`
+.header
+{
+  text-align: center;
+  height: 50px;
+  background: black;
+  color: #fff;
+  font-weight: bold;
+  line-height: 50px;
+}
+.form-content
+{
+  min-height: 150px;
+  padding: 5%;
+  border: 1px solid #ced4da;
+  margin-bottom: 2%;
+}
+.form-control{
+  border-radius:1.5rem;
+}
+.btnSubmit
+{
+  border:none;
+  border-radius:1.5rem;
+  padding: 1%;
+  width: 100%;
+  cursor: pointer;
+  background: black;
+  color: #fff;
+}
+  `],
   encapsulation: ViewEncapsulation.Emulated
 })
 export class AuthChangePasswordComponent {
 
-    @Output()
-    submit: EventEmitter<any> = new EventEmitter();
+  changeform: FormGroup;
+  emailSend: boolean;
+  errorEmailSend: boolean;
 
-    @Output()
-    changePage: EventEmitter<boolean> = new EventEmitter();
+  constructor(
+    private authenticationService: AuthenticationService,
+    private router: Router
+  ) {
+    this.emailSend = false;
+    this.errorEmailSend = false;
+    this.changeform = new FormGroup({
+      'email': new FormControl('', [Validators.required]),
+    });
+  }
 
-    credentials: CredentialsInterface = {
-            password: '',
-            repassword: ''
-        };
-    
-    changeform: FormGroup;
-
-    constructor(
-        private authenticationService: AuthenticationService,
-        private formBuilder: FormBuilder){
-        this.credentials = {
-            password: '',
-            repassword: ''
-        };
+  forgorPassword() {
+    if (this.changeform.valid) {
+      this.authenticationService.resetPassword(this.changeform.get('email').value).then((user) => {
+        this.emailSend = true;
+      }).catch(error => this.errorEmailSend = true);
     }
+  }
 
-    ngOnInit(): any {
-        this.changeform = this.formBuilder.group({
-            password: ['', [Validators.required, Validators.minLength(3)]],
-            repassword: ['', [Validators.required, Validators.minLength(3)]]
-        });
-    }
-
-    onSubmit() {
-        this.authenticationService
-            .changePassword(this.changeform.value)
-            .subscribe((result: any) => {
-                this.submit.emit(result);
-            });
-    }
-
-    loginAccount() {
-        this.changePage.emit(true);
-    }
-
+  goToHome() {
+    this.router.navigate(['/']);
+  }
 }
