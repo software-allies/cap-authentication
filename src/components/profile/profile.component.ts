2ecebb77be8187477a602271f236c20249f60c54
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2019-11-13 10:46:36 -0600

    CAP-94  src/components/profile/profile.component.ts Create a new functionality to see the details of the user's account and also be able to edit it.

--- src/components/profile/profile.component.ts
+++ src/components/profile/profile.component.ts
@@ -1,70 +1,125 @@
-import { Component, Output, EventEmitter, ViewEncapsulation } from '@angular/core'; 
-import { FormGroup, FormBuilder, Validators } from "@angular/forms";
-import { CredentialsInterface } from './../../interfaces/credentials.interface';
-
+import { Component, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
+import { FormGroup, FormControl, Validators, } from '@angular/forms';
 import { AuthenticationService } from '../../services/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
-    selector: "auth-app-edit-profile",
-    template: `
-    <ion-grid>
-        <ion-row>
-        <ion-col  col-12 col-xl-8 offset-xl-2 col-lg-10 offset-lg-1>
-            <ion-list>
-                <form [formGroup]="editForm" (ngSubmit)="onSubmit()">
-                    <ion-item>
-                        <ion-label stacked primary>Username</ion-label>
-                        <ion-input [(ngModel)]="credentials.username" formControlName="username"
-                                type="text" id="username" spellcheck="false" autocapitalize="off" ngDefaultControl>
-                        </ion-input>
-                    </ion-item>
-                    
-                    <button ion-button type="submit" block full primary [disabled]="!editForm.valid">Edit</button>
-                </form>
-            </ion-list>
-        </ion-col>
-        </ion-row>
-    </ion-grid> `,
-    
-    styles: [``],
+  selector: "auth-app-profile",
+  template: `
+  <div class="container register-form">
+    <div class="form">
+      <div class="header">
+          <p>Profile</p>
+      </div>
+      <div class="form-content" *ngIf="user">
+        <form [formGroup]="profileUserForm" (ngSubmit)="editProfile()">
+          <div  class="row">
+            <div class="col-md-6 mb-4">
+              <div class="form-group" >
+                <input  type="text"
+                        class="form-control"
+                        placeholder="Full name *"
+                        formControlName="displayName"/>
+              </div>
+
+              <div class="form-group" >
+                <input  type="text"
+                        class="form-control"
+                        placeholder="Phone number *"
+                        formControlName="phoneNumber"/>
+              </div>
+
+              <button type="submit"
+                class="btnSubmit"
+                [disabled]="!(profileUserForm.get('displayName').touched || profileUserForm.get('phoneNumber').touched)">Edit Profile</button>
+            </div>
+            <div class="col-md-6">
+              <div class="ml-5">
+                <p> Email : {{user.email}}
+                <p> Authentication Method: {{user.providerData[0].providerId}} </p>
+                <p> UID User: {{user.providerData[0].uid}} </p>
+                <p *ngIf="user.emailVerified"> Verified Email : Yes
+                <p *ngIf="!user.emailVerified"> Verified Email : No
+                <p> Creation Date: {{user.metadata.creationTime | date:'medium'}}
+                <p> Last SignIn : {{user.metadata.lastSignInTime | date:'medium'}}
+              </div>
+            </div>
+
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>`,
+  styles: [`
+  .header
+{
+  text-align: center;
+  height: 50px;
+  background: black;
+  color: #fff;
+  font-weight: bold;
+  line-height: 50px;
+}
+.form-content
+{
+  min-height: 150px;
+  padding: 5%;
+  border: 1px solid #ced4da;
+  margin-bottom: 2%;
+}
+.form-control{
+  border-radius:1.5rem;
+}
+.btnSubmit
+{
+  border:none;
+  border-radius:1.5rem;
+  padding: 1%;
+  width: 100%;
+  cursor: pointer;
+  background: black;
+  color: #fff;
+}
+`],
   encapsulation: ViewEncapsulation.Emulated
 })
 export class AuthEditComponent {
 
-    @Output()
-    submit: EventEmitter<any> = new EventEmitter();
+  profileUserForm: FormGroup;
 
-    @Output()
-    changePage: EventEmitter<boolean> = new EventEmitter();
+  private user: any;
+  constructor(
+    private authenticationService: AuthenticationService,
+    private router: Router,
+  ) {
 
-    credentials: CredentialsInterface = {
-        username:'',
-        password: ''    //este credencial es obligatoria 
-    };
+  }
 
-    editForm: FormGroup;
-
-    constructor(
-        private authenticationService: AuthenticationService,
-        public formBuilder: FormBuilder) {
-            this.credentials = {
-                username: '',
-                password: ''
-            };
-    }
-
-    ngOnInit(): any {
-        this.editForm = this.formBuilder.group({
-            username: ['', [Validators.required, Validators.minLength(3)]],
-            
+  ngOnInit() {
+    this.authenticationService.currentUser.subscribe((user) => {
+      if (user) {
+        this.user = user;
+        this.profileUserForm = new FormGroup({
+          'displayName': new FormControl(user.displayName, []),
+          'phoneNumber': new FormControl(user.phoneNumber, [])
         });
-    }
+      } else {
+        this.router.navigate(['/']);
+      }
+    });
+  }
 
-    onSubmit() {
-        this.authenticationService
-            .editProfile(this.credentials)
-            .subscribe((result: any) => {
-                this.submit.emit(result);
-            });
-    }
+  editProfile() {
+    this.authenticationService.currentUser.subscribe((user) => {
+      if ( user ) {
+        this.user.phoneNumber = this.profileUserForm.get('phoneNumber').value;
+        this.user.displayName = this.profileUserForm.get('displayName').value;
+        this.authenticationService.updateProfile(user).then((response) => {
+          console.log(response);
+        });
+      } else {
+        this.router.navigate(['/']);
+      }
+    });
+  }
 }
