75587437284a2d356f8a510ee339b8e3e32a7421
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2019-11-13 10:31:47 -0600

    CAP-94 src/services/config.service.ts Update the configuration for the credentials.

--- src/services/config.service.ts
+++ src/services/config.service.ts
@@ -1,19 +1,26 @@
-import { IConfig } from './../interfaces/config.interface';
 import { Injectable, Optional } from '@angular/core';
-
-
 @Injectable()
 export class ConfigService {
 
-    apiUrl: string;
-    loginEndpoint: string;
+  apiKey: string;
+  authDomain: string;
+  databaseURL: string;
+  projectId: string;
+  storageBucket: string;
+  messagingSenderId: string;
+  appId: string;
+  measurementId: string;
 
-
-    constructor(@Optional() config: IConfig) {
-        if (config) {
-            this.apiUrl = config.apiUrl;
-            this.loginEndpoint = config.loginEndpoint;
-            
-        }
+  constructor(@Optional() config: any) {
+    if (config) {
+      this.apiKey = config.apiKey;
+      this.authDomain = config.authDomain;
+      this.databaseURL = config.databaseURL;
+      this.projectId = config.projectId;
+      this.storageBucket = config.storageBucket;
+      this.messagingSenderId = config.messagingSenderId;
+      this.appId = config.appId;
+      this.measurementId = config.measurementId;
     }
+  }
 }
