c8d66ac877aad8c12b0fd2c99202154c3d1c1239
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2019-11-15 16:19:24 -0600

    CAP-94 src/authentication-auth0.module.ts Create the module for the Auth0 version.

---
+++ src/authentication-auth0.module.ts
@@ -0,0 +1,60 @@
+import { NgModule, ModuleWithProviders, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ConfigService } from './services/config.service';
+import { IConfig } from './interfaces/config.interface';
+import { AuthenticationAuth0Service } from './services/authentication-auth0.service';
+import { CommonModule } from "@angular/common";
+
+import { AuthRegisterAuth0Component } from './components/register/register-auth0.component';
+import { AuthProfileAuth0Component } from './components/profile/profile-auth0.component';
+import { AuthLoginAuth0Component } from './components/login/login-auth0.component';
+import { logOutAuth0Component } from './components/log-out/log-out-Auth0.component';
+import { AuthChangePasswordComponent } from './components/change-password/change-password-auth0.component';
+
+@NgModule({
+  declarations: [
+    AuthChangePasswordComponent,
+    AuthRegisterAuth0Component,
+    AuthProfileAuth0Component,
+    AuthLoginAuth0Component,
+    logOutAuth0Component
+  ],
+  imports: [
+    HttpClientModule,
+    ReactiveFormsModule,
+    CommonModule,
+    FormsModule,
+  ],
+  exports: [
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    CommonModule,
+    AuthChangePasswordComponent,
+    AuthRegisterAuth0Component,
+    AuthProfileAuth0Component,
+    AuthLoginAuth0Component,
+    logOutAuth0Component
+  ],
+  providers: [
+    AuthenticationAuth0Service,
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
+})
+export class AuthenticationAuth0Module {
+  static forRoot(config: IConfig): ModuleWithProviders {
+    return {
+      ngModule: AuthenticationAuth0Module,
+      providers: [
+        {
+          provide: ConfigService,
+          useValue: config,
+        },
+      ]
+    };
+  }
+}
+
