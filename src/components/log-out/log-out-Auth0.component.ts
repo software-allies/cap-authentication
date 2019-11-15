ca33bd77f1a68b03a9903a87633316af18d42f83
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2019-11-15 16:33:22 -0600

    CAP-94 src/components/login/login-auth0.component.ts create the LogOut component with its template and all the functionality so that the user deletes his information saved in the LocalStorage.

---
+++ src/components/log-out/log-out-Auth0.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, Inject, PLATFORM_ID} from '@angular/core';
+import { Router } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
+
+@Component({
+  selector: 'auth-app-log-out-auth0',
+  template:
+    `
+    <div class="d-flex justify-content-center">
+      <div class="spinner-grow" style="width: 6rem; height: 6rem;" role="status">
+        <span class="sr-only">Loading...</span>
+      </div>
+    </div>
+    `,
+  styles: [``],
+})
+
+export class logOutAuth0Component implements OnInit {
+
+  constructor(
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId
+  ) {
+    this.logOutUser();
+  }
+
+  ngOnInit() { }
+
+  logOutUser() {
+    if (isPlatformBrowser(this.platformId) && localStorage.getItem('User')) {
+      console.log('dentro');
+      localStorage.removeItem('User');
+    }
+    this.router.navigate(['/']);
+  }
+
+}
