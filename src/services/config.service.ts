06549e84b27893de08a471617881151a8537a3e2
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2019-11-15 16:18:20 -0600

    CAP-94 src/services/config.service.ts Initialize the module obtaining the credentials with a service to be able to use them within the components.

--- src/services/config.service.ts
+++ src/services/config.service.ts
@@ -2,25 +2,15 @@ import { Injectable, Optional } from '@angular/core';
 @Injectable()
 export class ConfigService {
 
-  apiKey: string;
-  authDomain: string;
-  databaseURL: string;
-  projectId: string;
-  storageBucket: string;
-  messagingSenderId: string;
-  appId: string;
-  measurementId: string;
+  domain: string;
+  clientId: string;
+  clientSecret: string;
 
   constructor(@Optional() config: any) {
     if (config) {
-      this.apiKey = config.apiKey;
-      this.authDomain = config.authDomain;
-      this.databaseURL = config.databaseURL;
-      this.projectId = config.projectId;
-      this.storageBucket = config.storageBucket;
-      this.messagingSenderId = config.messagingSenderId;
-      this.appId = config.appId;
-      this.measurementId = config.measurementId;
+      this.domain = config.domain;
+      this.clientId = config.clientId;
+      this.clientSecret = config.clientSecret;
     }
   }
 }
