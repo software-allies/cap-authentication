3c139ff60b3bf5a4386d3ee8dcf17bfe67ee6922
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2020-01-29 13:56:46 -0600

    CAP-308 src/ Group all module content within the /lib folder.

---
+++ src/public-api.ts
@@ -0,0 +1,12 @@
+/*
+ * Public API Surface of cap-authentication
+ */
+export * from './lib/authentication-auth0.module';
+export * from './lib/services/authentication-auth0.service';
+export * from './lib/services/config.service';
+export * from './lib/components/change-password/change-password-auth0.component';
+export * from './lib/components/log-out/log-out-Auth0.component';
+export * from './lib/components/login/login-auth0.component';
+export * from './lib/components/profile/profile-auth0.component';
+export * from './lib/components/register/register-auth0.component';
+export * from './lib/interfaces/config.interface';
