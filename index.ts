5062a7f6025667d5f8d525c54024fabf853c53cd
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2020-01-29 13:06:59 -0600

    CAP-308 index.ts Import all module files.

--- index.ts
+++ index.ts
@@ -1,5 +1,10 @@
-export * from './src/authentication.module';
-export * from './src/services/authentication.service';
-export * from './src/components/login/login.component';
-export * from './src/components/register/register.component';
-export * from './src/components/change-password/change-password.component';
+
+export * from './src/authentication-auth0.module';
+export * from './src/components/change-password/change-password-auth0.component';
+export * from './src/components/log-out/log-out-Auth0.component';
+export * from './src/components/login/login-auth0.component';
+export * from './src/components/profile/profile-auth0.component';
+export * from './src/components/register/register-auth0.component';
+export * from './src/interfaces/config.interface';
+export * from './src/services/authentication-auth0.service';
+export * from './src/services/config.service';
