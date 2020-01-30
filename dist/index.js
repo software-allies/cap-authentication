d3dd3ef0bcfa26721859c9848971574560febd84
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2020-01-30 08:59:09 -0600

    CAP-308 ./ Update all the necessary files and configurations so that it is working with ng library (@angular-devkit/build-ng-packagr and ng-packagr).

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-export * from './src/authentication.module';
-export * from './src/services/authentication.service';
-export * from './src/components/login/login.component';
-export * from './src/components/register/register.component';
-export * from './src/components/change-password/change-password.component';
-//# sourceMappingURL=index.js.map
