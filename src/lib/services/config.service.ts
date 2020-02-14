4cf7f9f09f99c0cdfcbcd51227967cee2aef4fa4
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2020-02-14 15:00:37 -0600

    CAP-349 src/lib/services/config.service.ts Configure the module initiation service to accept another parameter and can be used throughout the module.

--- src/lib/services/config.service.ts
+++ src/lib/services/config.service.ts
@@ -1,16 +1,19 @@
 import { Injectable, Optional } from '@angular/core';
+import { IConfig } from '../interfaces/config.interface';
 @Injectable()
 export class ConfigService {
 
   domain: string;
   clientId: string;
   clientSecret: string;
+  endPoint: string;
 
-  constructor(@Optional() config: any) {
+  constructor(@Optional() config: IConfig) {
     if (config) {
       this.domain = config.domain;
       this.clientId = config.clientId;
       this.clientSecret = config.clientSecret;
+      this.endPoint = config.endPoint;
     }
   }
 }
