f5fd5639afd6b173d425328856eaa87d9b3d2e3a
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2020-03-13 11:00:53 -0600

    CAP-308 src/lib/components/login/login.component.ts Create the link to redirect to the component module forgot-password.

--- src/lib/components/login/login.component.ts
+++ src/lib/components/login/login.component.ts
@@ -15,29 +15,43 @@ import { Router } from '@angular/router';
       <form [formGroup]="loginUserForm" (ngSubmit)="loginUser()">
         <div class="row">
           <div class="col-md-6">
-              <div class="form-group" >
-                  <input  type="text"
-                          class="form-control"
-                          placeholder="Email address *"
-                          [ngClass]="{'invalidField':(!loginUserForm.get('email').valid && loginUserForm.get('email').touched) || (validatedForm && !loginUserForm.get('email').valid)}"
-                          formControlName="email"/>
-              </div>
-              <div class="form-group">
-                  <input  type="password"
-                          class="form-control"
-                          placeholder="Password *"
-                          [ngClass]="{'invalidField':(!loginUserForm.get('password').valid && loginUserForm.get('password').touched) || (validatedForm && !loginUserForm.get('password').valid)}"
-                          formControlName="password"/>
-                  <small *ngIf="!loginUserForm.get('password').valid && loginUserForm.get('password').touched" class="form-text text-center text-muted">
-                    Your password must be 8-20 characters long, contain letters and numbers and the first letter has to be uppercase.
-                  </small>
+            <div class="form-group">
+              <small class="form-text">
+                Email
+              </small>
+                <input  type="text"
+                        class="form-control"
+                        [ngClass]="{'invalidField':(!loginUserForm.get('email').valid && loginUserForm.get('email').touched) || (validatedForm && !loginUserForm.get('email').valid)}"
+                        formControlName="email"/>
+            </div>
 
-                <div *ngIf="userNotValid"  class="form-control-feeback text-danger text-center">
-                  invalid email or password
-                </div>
-                <div *ngIf="socialMedia"  class="form-control-feeback text-danger text-center">
-                  At the moment authentication with Social networks is under development, try by Email
-                </div>
+            <div class="form-group row">
+              <div class="col-6">
+                <small class="form-text">
+                  Password
+                </small>
+              </div>
+
+              <div class="col-6">
+                <small class="form-text text-right">
+                  <a href="/auth/forgot-password"> Forgot password? </a>
+                </small>
+              </div>
+
+              <input  type="password"
+                      class="form-control"
+                      [ngClass]="{'invalidField':(!loginUserForm.get('password').valid && loginUserForm.get('password').touched) || (validatedForm && !loginUserForm.get('password').valid)}"
+                      formControlName="password"/>
+              <small *ngIf="!loginUserForm.get('password').valid && loginUserForm.get('password').touched" class="form-text text-center text-muted">
+                Your password must be 8-20 characters long, contain letters and numbers and the first letter has to be uppercase.
+              </small>
+
+              <div *ngIf="userNotValid"  class="form-control-feeback text-danger text-center">
+                invalid email or password
+              </div>
+              <div *ngIf="socialMedia"  class="form-control-feeback text-danger text-center">
+                At the moment authentication with Social networks is under development, try by Email
+              </div>
             </div>
           </div>
             <div class="col-md-6">
