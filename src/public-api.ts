5e2644b9daa8be4887c0474809109eadb61adbcd
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2021-01-20 15:38:36 -0600

    CAP src/public-api.ts Export the components to be used within an application.

--- src/public-api.ts
+++ src/public-api.ts
@@ -10,6 +10,7 @@ export * from './lib/components/change-password/change-password.component';
 export * from './lib/components/login/login.component';
 export * from './lib/components/profile/profile.component';
 export * from './lib/components/register/register.component';
+export * from './lib/components/forgot/forgot.component';
 export * from './lib/interfaces/config.interface';
 export * from './lib/components/verify/verify.component';
 export * from './lib/interfaces/authentication.interface';
